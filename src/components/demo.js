f13ab934d4c46f8b1c4895646be544494923413e
Noman Hasan <nomanbinhussein@gmail.com>
2017-11-16 15:11:26 +0600

    ComposeMap Succeded

--- src/components/demo.js
+++ src/components/demo.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
+import './demo.css'
 
-import {BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar} from 'recharts'
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts'
 
 import * as R from 'ramda'
 
-import {CARS} from '../data/Cars'
+import { CARS } from '../data/Cars'
 
 const mutateObj = obj => key => value => {
     obj[key] = value
 }
 
-const valuePerPrice = (value, price) => value / price
 
-const calulateVPP = c => Object.assign({}, c, {vpp: valuePerPrice(c.horsepower, c.dollar_value)} )
+const compose = (...fns) => fns.reduce((prev, next) => value => prev(next(value)), value => value)
+
+const composeMap = (...fns) => fns.reduce((prev, next) => value => prev(value.map(next)), value => value)
+
+
+const valuePerPrice = (value, price) => value / price
 
 const VPP = c => c.vpp
 
-const calculateRV = x => { x.rv = x.vpp * (100 / max); return x}
+
+const calculateVPP = c => Object.assign({}, c, { vpp: valuePerPrice(c.horsepower, c.dollar_value) })
+const calculateRV = x => { x.rv = x.vpp * (100 / max); return x }
 
 window.CARS = CARS
 window.R = R
-window.calulateVPP = calulateVPP
+window.calulateVPP = calculateVPP
 window.VPP = VPP
 
-let cars = R.map(calulateVPP, CARS)
+let cars = R.map(calculateVPP, CARS)
 let vpps = cars.map(VPP)
 let max = Math.max(...vpps)
 let rv = R.map(calculateRV, cars)
@@ -31,31 +38,56 @@ let rv = R.map(calculateRV, cars)
 console.log(cars, vpps, max, rv)
 console.log(rv)
 
-let res  = R.compose( Math.max(R.map(VPP,)) , R.map(calulateVPP, CARS))
+let res = R.compose(Math.max(R.map(VPP, )), R.map(calculateVPP, CARS))
+
+compose(calculateRV, calculateVPP)
 
 console.log(res)
 
+
+
+// Exercise 1
+var isLastInStock = function (cars) {
+    var last_car = R.last(cars);
+    return R.prop('in_stock', last_car);
+};
+
+var isLastInStock2 = 
+
+console.log(isLastInStock(CARS))
+
+
+
+
+
+
+
+
 class Demo extends Component {
-    
+
     render() {
         let data = rv
         return (
-            Chart(data, 'rv', 900, 300)
+            <div className="chart-list">
+                {['vpp', 'rv'].map(key => {
+                    return Chart(data, key, 1000, 300)
+                })}
+            </div>
         );
     }
 }
 
 export default Demo;
 
-const Chart = (data, key, width=500, height=500) => { 
+const Chart = (data, key, width = 500, height = 500) => {
     return (
         <BarChart width={width} height={height} data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey={key} fill="#8884d8" />
-      </BarChart>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey={key} fill="#8884d8" />
+        </BarChart>
     );
 }
